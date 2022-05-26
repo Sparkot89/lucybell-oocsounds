88900d102d47dfb217c1ed69f987b46ef883b2b0
ALEJANDRO PRIETO GARCÍA <aprieto@viewnext.com>
2022-05-26 22:54:03 +0200

    cambios

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,23 +1,25 @@
 import '../App.css';
 
 function Button(props) {
-  return (
-    <div className="button-container">
-        <a onClick={(event)=>{
-            event.preventDefault();
-            let sound = new Audio(`https://raw.githubusercontent.com/Sparkot89/lucybell-oocsounds/main/public/audios/${props.sound.id}.mp3`);
-            sound.play();
-        }} 
-        onKeyDown={(event)=>{
-            event.preventDefault();
-        }}
-        href="/" className="button">
-            <p>
-                {props.sound.name}
-            </p>
-        </a>
-    </div>
-  );
+    debugger;
+    let isNew = props.sound.date > new Date().getTime() - (1000 * 60 * 60 * 24 * 3);
+    return (
+        <div className="button-container">
+            <a onClick={(event) => {
+                event.preventDefault();
+                let sound = new Audio(`https://raw.githubusercontent.com/Sparkot89/lucybell-oocsounds/main/public/audios/${props.sound.id}.mp3`);
+                sound.play();
+            }}
+                onKeyDown={(event) => {
+                    event.preventDefault();
+                }}
+                href="/" className={isNew ? "button newAudio" : "button"} >
+                <p>
+                    {props.sound.name}
+                </p>
+            </a>
+        </div>
+    );
 }
 
 export default Button;
